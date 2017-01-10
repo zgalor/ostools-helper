89552b79ef2b14490191963a44a83489a2518a35
Zohar Galor <zgalor@redhat.com>
2017-01-10 20:02:59 +0200

    adding docker run files

---
+++ oso-host-monitoring-setup/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Use this command to run oso-centos7-host-monitoring image after you setup
+# ./container_setup/monitoring-config.yml with your local definitions
+
+CONFIG_SOURCE=$(readlink -f ./container_setup)
+sudo docker run --rm=true -it --name oso-centos7-host-monitoring \
+           --privileged                                     \
+           --pid=host                                       \
+           --net=host                                       \
+           --ipc=host                                       \
+           -v /etc/localtime:/etc/localtime:ro              \
+           -v /sys:/sys:ro                                  \
+           -v /sys/fs/selinux                               \
+           -v /var/lib/docker:/var/lib/docker:ro            \
+           -v /var/lib/docker/volumes/shared:/shared:rw     \
+           -v /var/run/docker.sock:/var/run/docker.sock     \
+           -v ${CONFIG_SOURCE}:/container_setup:ro \
+           --memory 512m \
+           zgalor/oso-centos7-host-monitoring $@
